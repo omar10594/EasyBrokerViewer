34e85b96f391baa6a07ca6eb994650e8fcac24e1
Guillermo Lopez <omar10594@hotmail.com>
2025-02-22 16:53:24 +0000

    Feat: Properties view from API
    Implemented the properties index page now using the data from the API
    
    updated default pagination limit to 10, to improve the view

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,7 @@
 class PropertiesController < ApplicationController
   def index
+    properties_data = EasyBrokerApi::Properties.all(page: params[:page])
+    @properties = properties_data["content"]
+    @pagination = properties_data["pagination"]
   end
 end
--- app/models/easy_broker_api/properties.rb
+++ app/models/easy_broker_api/properties.rb
@@ -1,6 +1,6 @@
 module EasyBrokerApi
   class Properties < Base
-    def self.all(page: 1, limit: 8)
+    def self.all(page: 1, limit: 10)
       response = get("/properties", query: { page: page, limit: limit })
       response.parsed_response
     end
